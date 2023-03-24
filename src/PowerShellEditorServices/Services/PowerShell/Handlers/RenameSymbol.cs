240ba602aa57de93db9a6898140f0099ec379c2e
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2023-03-24 21:19:56 +1100

    getting closer still in dev, just gotta do the hard bit now

--- src/PowerShellEditorServices/Services/PowerShell/Handlers/RenameSymbol.cs
+++ src/PowerShellEditorServices/Services/PowerShell/Handlers/RenameSymbol.cs
@@ -2,12 +2,16 @@
 // Licensed under the MIT License.
 
 using System.Management.Automation;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using OmniSharp.Extensions.JsonRpc;
+using Microsoft.PowerShell.EditorServices.Services.Symbols;
 using Microsoft.PowerShell.EditorServices.Services.PowerShell;
-
+using Microsoft.PowerShell.EditorServices.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.PowerShell.EditorServices.Services.TextDocument;
 namespace Microsoft.PowerShell.EditorServices.Handlers
 {
     [Serial, Method("powerShell/renameSymbol")]
@@ -15,9 +19,9 @@ namespace Microsoft.PowerShell.EditorServices.Handlers
 
     internal class RenameSymbolParams : IRequest<RenameSymbolResult>
     {
-        public bool FileName { get; set; }
-        public bool Line { get; set; }
-        public bool Column { get; set; }
+        public string FileName { get; set; }
+        public int Line { get; set; }
+        public int Column { get; set; }
         public bool WantsTextChanges { get; set; }
         public bool ApplyTextChanges { get; set; } = true;
         public string RenameTo { get; set; }
@@ -32,20 +36,39 @@ namespace Microsoft.PowerShell.EditorServices.Handlers
     {
         private readonly IInternalPowerShellExecutionService _executionService;
 
-        public RenameSymbolHandler(IInternalPowerShellExecutionService executionService) => _executionService = executionService;
+        private readonly ILogger _logger;
+        private readonly WorkspaceService _workspaceService;
+
+        public RenameSymbolHandler(IInternalPowerShellExecutionService executionService,
+        ILoggerFactory loggerFactory,
+            WorkspaceService workspaceService) {
+            _logger = loggerFactory.CreateLogger<RenameSymbolHandler>();
+            _workspaceService = workspaceService;
+            _executionService = executionService;
+            }
 
         public async Task<RenameSymbolResult> Handle(RenameSymbolParams request, CancellationToken cancellationToken)
         {
-
+            if (!_workspaceService.TryGetFile(request.FileName, out ScriptFile scriptFile))
+            {
+                _logger.LogDebug("Failed to open file!");
+                return null;
+            }
+            //scriptFile.ScriptAst.FindAll()
+            IEnumerable<SymbolReference> occurrences = SymbolsService.FindOccurrencesInFile(
+                scriptFile,
+                request.Line+1,
+                request.Column+1);
+            _logger.LogDebug("Testing");
             // TODO: Refactor to not rerun the function definition every time.
             PSCommand psCommand = new();
             psCommand
                 .AddScript("Return 'hello world'")
                 .AddStatement();
-            System.Collections.Generic.IReadOnlyList<string> result = await _executionService.ExecutePSCommandAsync<string>(psCommand, cancellationToken).ConfigureAwait(false);
+            IReadOnlyList<string> result = await _executionService.ExecutePSCommandAsync<string>(psCommand, cancellationToken).ConfigureAwait(false);
             return new RenameSymbolResult
             {
-                Text = result[0]
+                Text = "YOYOYO"
             };
         }
     }
